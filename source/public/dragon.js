d63bdb06127fbadb5f425ce6f030be729ace9e90
JoshNeely <joshua.a.neely@gmail.com>
2017-03-11 13:30:28 -0600

    Add simplify function. Add custom pattern. Add default Dragon pattern

--- source/public/dragon.js
+++ source/public/dragon.js
@@ -11,8 +11,9 @@ function DragonController($scope, $routeParams) {
   var ctx = canvas.getContext("2d");
 
   var background_color = "#111111";
-
   ctx.strokeStyle = "#ff00ff";
+
+  var pattern = [1,-1];
 
   p1 = {x:canvas.width*0.2, y:canvas.height*0.4};
   p2 = {x:canvas.width*0.8, y:canvas.height*0.4};
@@ -23,6 +24,7 @@ function DragonController($scope, $routeParams) {
   // ------ controls ------
   addEventListener('keydown', function(event) {    
     if(event.keyCode == 37) {         // left
+      simplify();
       redraw();
     }
     else if(event.keyCode == 39) {    // right
@@ -57,13 +59,56 @@ function DragonController($scope, $routeParams) {
     if (points.length <= 1)
       return;
 
-    var new_points = [];
+    var new_points = [ points[0] ];
     for (var i=0; i<points.length-1; i++) {
       var p1 = points[i];
       var p2 = points[i+1];
-      new_points.push( p1 );
 
-      // need to integrate utilities.js...
+      var dist = Math.sqrt( Math.pow(p1.x-p2.x,2) + Math.pow(p1.y-p2.y,2) );
+      var squared_dist = dist*dist;
+      var leg_dist = Math.sqrt(squared_dist / 2);
+
+      var angle = angle_radians(p1,p2);
+      var angle_change = (3.14159/4.0);
+
+      if (pattern[0] == 1) {
+        var new_angle = angle + angle_change;
+      } 
+      else if (pattern[0] == -1 ) {
+        var new_angle = angle - angle_change;
+      }
+      else {
+        var new_angle = angle;
+      }
+      pattern.unshift( pattern.pop() );
+
+      var xdist = leg_dist * Math.cos(new_angle);
+      var ydist = leg_dist * Math.sin(new_angle);
+
+      var middle_point = {x:p1.x+xdist, y:p1.y+ydist}; 
+
+      new_points.push( middle_point );
+      new_points.push( p2 );
+    }
+
+    points = new_points;
+  }
+
+  function simplify() {
+    if (!points.length || !(points.length % 2))
+      return;
+
+    var new_points = [];
+    for (var i=0; i<points.length; i++) {
+      if (!i || !(i%2)) {
+        new_points.push( points[i] );
+      }
+    }
+
+    points = new_points;
+
+    if (!(points.length % 2)) {
+      pattern.unshift( pattern.pop() );
     }
   }
 }
