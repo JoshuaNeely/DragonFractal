a6aac82fb0bc730f4c1adc5ebdc10246b6e67851
JoshNeely <joshua.a.neely@gmail.com>
2017-03-11 12:25:38 -0600

    Add logic to display points

--- source/public/dragon.js
+++ source/public/dragon.js
@@ -12,18 +12,58 @@ function DragonController($scope, $routeParams) {
 
   var background_color = "#111111";
 
-  ctx.fillStyle = "#ff00ff";
+  ctx.strokeStyle = "#ff00ff";
 
-  draw_background();
+  p1 = {x:canvas.width*0.2, y:canvas.height*0.4};
+  p2 = {x:canvas.width*0.8, y:canvas.height*0.4};
+  var points = [p1,p2];
 
-  ctx.beginPath();
-  ctx.moveTo(20,20);
-  ctx.lineTo(20,100);
-  ctx.lineTo(70,100);
-  ctx.stroke();
+  redraw(); 
 
+  // ------ controls ------
+  addEventListener('keydown', function(event) {    
+    if(event.keyCode == 37) {         // left
+      redraw();
+    }
+    else if(event.keyCode == 39) {    // right
+      deepen();
+      redraw();
+    }
+  });
+
+  // ------ functions ------
   function draw_background() {
     ctx.fillStyle = background_color;
     ctx.fillRect(0,0, canvas.width, canvas.height);
+  }
+
+  function redraw() {
+    draw_background();
+
+    if (!points.length)
+      return;
+
+    ctx.beginPath();
+    ctx.moveTo(points[0].x, points[0].y);
+
+    for (var i=1; i<points.length; i++) {
+      ctx.lineTo(points[i].x, points[i].y);
+    }
+
+    ctx.stroke();
+  }
+
+  function deepen() {
+    if (points.length <= 1)
+      return;
+
+    var new_points = [];
+    for (var i=0; i<points.length-1; i++) {
+      var p1 = points[i];
+      var p2 = points[i+1];
+      new_points.push( p1 );
+
+      // need to integrate utilities.js...
+    }
   }
 }
