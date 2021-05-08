4749691dd1bda9d659bb8df1e15ce542970325d5
JoshuaNeely <joshua.a.neely@gmail.com>
2021-05-08 15:08:58 -0500

    feat: add export / import from queryParamters

--- src/app/control-panel/control-panel.component.ts
+++ src/app/control-panel/control-panel.component.ts
@@ -12,7 +12,7 @@ import { Pattern } from '../pattern';
 export class ControlPanelComponent implements OnInit, AfterViewInit {
 
   iterations = 12;
-  humanFacingPattern = '1, 1';
+  humanFacingPattern = '1 1';
   rawPattern: Pattern = [];
   zoom = 100;
   panX = 0;
@@ -24,11 +24,48 @@ export class ControlPanelComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.loadFromUrl();
+  }
 
   ngAfterViewInit(): void {
     this.processPattern(this.humanFacingPattern);
     this.emitUpdates();
+  }
+
+  exportToUrl(): void {
+    const queryParams = new URLSearchParams(location.search);
+    queryParams.set('zoom', `${this.zoom}`);
+    queryParams.set('panx', `${this.panX}`);
+    queryParams.set('pany', `${this.panY}`);
+    queryParams.set('angle', `${this.angle}`);
+    queryParams.set('iterations', `${this.iterations}`);
+    queryParams.set('pattern', `${this.humanFacingPattern}`);
+    location.href = '?' + queryParams.toString();
+  }
+
+  private loadFromUrl(): void {
+    this.zoom = this.getNumericQueryParam('zoom', this.zoom);
+    this.panX = this.getNumericQueryParam('panx', this.panX);
+    this.panY = this.getNumericQueryParam('pany', this.panY);
+    this.angle = this.getNumericQueryParam('angle', this.angle);
+    this.iterations = this.getNumericQueryParam('iterations', this.iterations);
+    this.humanFacingPattern = this.getStringQueryParam('pattern', this.humanFacingPattern);
+  }
+
+  private getNumericQueryParam(paramName: string, defaultVal: number): number {
+    const queryParams = new URLSearchParams(location.search);
+    const stringVal = queryParams.get(paramName) || `${defaultVal}`;
+    const numericVal = parseInt(stringVal, 10);
+    const returnedVal = (typeof numericVal === 'number') ? numericVal : defaultVal;
+    return returnedVal;
+  }
+
+  private getStringQueryParam(paramName: string, defaultVal: string): string {
+    const queryParams = new URLSearchParams(location.search);
+    const stringVal = queryParams.get(paramName) || defaultVal;
+    const returnedVal = (typeof stringVal === 'string') ? stringVal : defaultVal;
+    return returnedVal;
   }
 
   updateIterations(eventTarget: any): void {
