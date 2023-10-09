a00995410d91e01da58375b4180220997040c776
JoshuaNeely <joshua.a.neely@gmail.com>
2023-10-09 16:56:26 -0500

    update angular and node dependencies

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-echo "Building new deployment..."
-npm run build-prod
-echo "Removing old deployment..."
-rm -rf /mnt/nfs/joshua-neely.com/core_public/dragon-fractals
-echo "Copying new deployment..."
-cp -r dist/dragon-fractals /mnt/nfs/joshua-neely.com/core_public/dragon-fractals
-echo "Done"
